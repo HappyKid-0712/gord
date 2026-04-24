841592095f640f67c0e896363d40f0a8a97bb1cd
6ICKO6OY <1397610285@qq.com>
2026-04-24 19:28:20 +0800

    优化控制台输出，增强错误提示和释义展示，添加音标和水印

--- internal/printer/console.go
+++ internal/printer/console.go
@@ -3,14 +3,44 @@ package printer
 import (
 	"fmt"
 	"gord/internal/model"
+
+	"github.com/fatih/color"
 )
 
 func PrintConsole(result model.DictResult, err error) {
+	errPen := color.New(color.FgHiRed, color.Bold)
+	wordPen := color.New(color.FgHiCyan, color.Bold)
+	phoneticPen := color.New(color.FgHiMagenta)
+	bulletPen := color.New(color.FgHiYellow)
+	sourcePen := color.New(color.FgHiBlack, color.Italic)
+
 	if err != nil {
-		fmt.Println(err)
+		errPen.Printf("\n查询错误❌：%s、\n\n", err)
 		return
 	}
-	fmt.Println(result.Meanings)
-	fmt.Println(result.Phonetic)
-	fmt.Println(result.Source)
+	wordPen.Printf("🔤 %s \n", result.Word)
+
+	// 2. 打印音标 (如果有的话)
+	if result.Phonetic != "" {
+		phoneticPen.Printf("[%s]\n", result.Phonetic)
+	} else {
+		fmt.Println()
+	}
+
+	// 分割线
+	color.HiBlack(" ----------------------------------------")
+
+	// 3. 遍历打印多重释义
+	for _, meaning := range result.Meanings {
+		bulletPen.Print("  • ")
+		fmt.Printf("%s\n", meaning)
+	}
+
+	fmt.Println()
+
+	// 4. 右下角水印
+	sourcePen.Printf("    (Powered by %s)\n", result.Source)
+	fmt.Println()
+	fmt.Println() //顶部空格流出呼吸感
+
 }
